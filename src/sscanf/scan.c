aad53a22d083f26cd6fc9d27879497f046b11fc3
AmogusGrepper <main@0x1f44.ru>
2026-01-30 12:37:10 +0300

    [ref] split scan decimal to unsigned and signed funcs

--- src/sscanf/scan.c
+++ src/sscanf/scan.c
@@ -4,14 +4,23 @@
 
 #include "../common/s21_format.h"
 
+// Public function declaration
+const char* scan_string_var(ScanState* state);
+
+// Static scan functions declaration
 static const char* scan_char(ScanState* state);
+
 static const char* scan_decimal(ScanState* state);
+static void assign_signed_decimal(ScanState* state, long long value);
+static void assign_unsigned_decimal(ScanState* state, unsigned long long value);
+
 static const char* scan_float(ScanState* state);
 static const char* scan_string(ScanState* state);
 static const char* scan_nread(ScanState* state);
 static const char* scan_pointer(ScanState* state);
 static const char* scan_symbol(ScanState* state);
 
+// Public function implementation
 const char* scan_string_var(ScanState* state) {
   switch (state->spec->var) {
     case VAR_CHAR:
@@ -42,6 +51,7 @@ const char* scan_string_var(ScanState* state) {
   return state->str;
 }
 
+// Static functions implementation
 static const char* scan_char(ScanState* state) {
   *(char*)state->param = *state->str;
   return state->str + 1;
@@ -71,42 +81,51 @@ static const char* scan_decimal(ScanState* state) {
   }
 
   if (state->spec->val == 'u') {
-    switch (state->spec->len) {
-      case LEN_DEFAULT:
-        *(unsigned int*)(state->param) = value;
-        break;
-      case LEN_SHORT:
-        *(unsigned short*)(state->param) = value;
-        break;
-      case LEN_LONG:
-        *(unsigned long*)(state->param) = value;
-        break;
-      case LEN_LONG_LONG:
-        *(unsigned long long*)(state->param) = value;
-        break;
-      default:
-        break;
-    }
+    assign_unsigned_decimal(state, value);
   } else {
-    long long signed_value = sign * (long long)value;
-    switch (state->spec->len) {
-      case LEN_DEFAULT:
-        *(int*)(state->param) = (int)signed_value;
-        break;
-      case LEN_SHORT:
-        *(short*)(state->param) = (short)signed_value;
-        break;
-      case LEN_LONG:
-        *(long*)(state->param) = (long)signed_value;
-        break;
-      case LEN_LONG_LONG:
-        *(long long*)(state->param) = signed_value;
-        break;
-      default:
-        break;
-    }
+    assign_signed_decimal(state, sign * (long long)(value));
   }
+
   return state->str;
+}
+
+static void assign_signed_decimal(ScanState* state, long long value) {
+  switch (state->spec->len) {
+    case LEN_DEFAULT:
+      *(int*)(state->param) = (int)value;
+      break;
+    case LEN_SHORT:
+      *(short*)(state->param) = (short)value;
+      break;
+    case LEN_LONG:
+      *(long*)(state->param) = (long)value;
+      break;
+    case LEN_LONG_LONG:
+      *(long long*)(state->param) = (long long)value;
+      break;
+    default:
+      break;
+  }
+}
+
+static void assign_unsigned_decimal(ScanState* state,
+                                    unsigned long long value) {
+  switch (state->spec->len) {
+    case LEN_DEFAULT:
+      *(unsigned int*)(state->param) = value;
+      break;
+    case LEN_SHORT:
+      *(unsigned short*)(state->param) = value;
+      break;
+    case LEN_LONG:
+      *(unsigned long*)(state->param) = value;
+      break;
+    case LEN_LONG_LONG:
+      *(unsigned long long*)(state->param) = value;
+      break;
+    default:
+      break;
+  }
 }
 
 static const char* scan_float(ScanState* state) { return state->str; }
