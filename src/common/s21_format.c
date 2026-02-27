5ba2ae3835447475ebe2e4834c5bd8cec8959503
Gasem Al Mazharish <al.mazharish.g@phystech.edu>
2026-02-27 22:49:31 +0300

    Expand s21_sprintf support for required format specifiers

--- src/common/s21_format.c
+++ src/common/s21_format.c
@@ -1,7 +1,16 @@
 #include "s21_format.h"
 #include "../string/s21_string.h"
-
 #include <ctype.h>
+#include <stddef.h>
+
+static bool s21_contains_char(const char *set, char c) {
+  if (set == NULL) return false;
+  while (*set != '\0') {
+    if (*set == c) return true;
+    set++;
+  }
+  return false;
+}
 
 // Helper static functions declaration
 static const char *parse_flags(const char *format, s21_specifier *spec);
@@ -41,10 +50,10 @@ static const char *parse_width(const char *format, s21_specifier *spec) {
     spec->width_from_arg = true;
     spec->has_width = true;
     format++;
-  } else if (isdigit(*format)) {
+  } else if (isdigit((unsigned char)*format)) {
     spec->has_width = true;
     spec->width = 0;
-    while (isdigit(*format)) {
+    while (isdigit((unsigned char)*format)) {
       spec->width = spec->width * 10 + (*format - '0');
       format++;
     }
@@ -63,9 +72,9 @@ static const char *parse_precision(const char *format, s21_specifier *spec) {
     if (*format == '*') {
       spec->precision_from_arg = true;
       format++;
-    } else if (isdigit(*format)) {
+    } else if (isdigit((unsigned char)*format)) {
       spec->precision = 0;
-      while (isdigit(*format)) {
+      while (isdigit((unsigned char)*format)) {
         spec->precision = spec->precision * 10 + (*format - '0');
         format++;
       }
@@ -103,9 +112,9 @@ static const char *parse_spec_letter(const char *format, s21_specifier *spec) {
 
   if (*format == 'c')
     spec->var = VAR_CHAR;
-  else if (s21_strchr("diouxX", *format))
+  else if (s21_contains_char("diouxX", *format))
     spec->var = VAR_DECIMAL;
-  else if (s21_strchr("eEfgG", *format))
+  else if (s21_contains_char("eEfgG", *format))
     spec->var = VAR_FLOAT;
   else if (*format == 's')
     spec->var = VAR_STRING;
--- src/sprintf/s21_sprintf.c
+++ src/sprintf/s21_sprintf.c
@@ -1,74 +1,98 @@
+#include "s21_sprintf.h"
+
 #include "../common/s21_format.h"
-#include "../string/s21_string.h"
+#include "s21_sprintf_helpers.h"
 
-#include <stdarg.h>
+static int s21_append_fragment(char **dst, const char *src, int src_len) {
+  if (dst == S21_NULL || *dst == S21_NULL || src == S21_NULL || src_len < 0) {
+    return -1;
+  }
 
-static void handle_spec_error(s21_specifier *spec);
-static char *handle_char(char *str, s21_specifier *spec, char c);
-static char *handle_decimal(char *str, s21_specifier *spec, ...);
-static char *handle_float(char *str, s21_specifier *spec, ...);
-static char *handle_nread(char *str, s21_specifier *spec, int n);
-static char *handle_pointer(char *str, s21_specifier *spec, void *p);
-static char *handle_string(char *str, s21_specifier *spec, const char *s);
-static char *handle_percent(char *str, s21_specifier *spec);
+  for (int i = 0; i < src_len; i++) {
+    **dst = src[i];
+    (*dst)++;
+  }
+
+  return 0;
+}
+
+static int s21_handle_specifier(char *fragment, s21_size fragment_size,
+                                s21_specifier *spec, va_list *ap) {
+  int len = -1;
+
+  switch (spec->var) {
+    case VAR_CHAR:
+      len = s21_format_char(fragment, fragment_size, va_arg(*ap, int), spec);
+      break;
+    case VAR_STRING:
+      len = s21_format_string(fragment, fragment_size, va_arg(*ap, const char *),
+                              spec);
+      break;
+    case VAR_DECIMAL:
+      if (spec->val == 'd' || spec->val == 'i') {
+        len = s21_format_signed_decimal(fragment, fragment_size, spec, ap);
+      } else if (spec->val == 'u' || spec->val == 'o' || spec->val == 'x' ||
+                 spec->val == 'X') {
+        len = s21_format_unsigned(fragment, fragment_size, spec, ap);
+      }
+      break;
+    case VAR_FLOAT:
+      if (spec->val == 'f') {
+        len = s21_format_float_fixed(fragment, fragment_size, spec, ap);
+      }
+      break;
+    case VAR_POINTER:
+      len = s21_format_pointer(fragment, fragment_size, spec, ap);
+      break;
+    case VAR_SYMBOL:
+      len = s21_format_percent(fragment, fragment_size, spec);
+      break;
+    default:
+      break;
+  }
+
+  return len;
+}
 
 int s21_sprintf(char *str, const char *format, ...) {
-  if (str == S21_NULL || format == S21_NULL) return 0;
+  if (str == S21_NULL || format == S21_NULL) return -1;
 
   va_list ap;
   va_start(ap, format);
 
   const char *fmt = format;
+  char *out = str;
   char *start = str;
 
   while (*fmt != '\0') {
     if (*fmt != '%') {
-      *str = *fmt;
-      str++;
+      *out = *fmt;
+      out++;
       fmt++;
-    } else {
-      fmt++;
-      s21_specifier spec = {};
-      fmt = s21_parse_spec(fmt, &spec);
-      s21_validate_spec(&spec);
+      continue;
+    }
 
-      if (!spec.valid) {
-        handle_spec_error(&spec);
-        va_end(ap);
-        return -1;
-      }
+    fmt++;
+    s21_specifier spec = {0};
+    fmt = s21_parse_spec(fmt, &spec);
+    s21_validate_spec(&spec);
 
-      switch (spec.var)
-      {
-      case VAR_CHAR:
-        /* code */
-        break;
-      case VAR_DECIMAL:
-        /* code */
-        break;
-      case VAR_FLOAT:
-        /* code */
-        break;
-      case VAR_NREAD:
-        /* code */
-        break;
-      case VAR_POINTER:
-        /* code */
-        break;
-      case VAR_STRING:
-        /* code */
-        break;
-      case VAR_SYMBOL:
-        /* code */
-        break;
-      
-      default:
-        break;
-      }
+    if (!spec.valid) {
+      va_end(ap);
+      return -1;
+    }
+
+    s21_read_width_precision_from_args(&spec, &ap);
+
+    char fragment[S21_FLOAT_BUF] = {0};
+    int fragment_len = s21_handle_specifier(fragment, sizeof(fragment), &spec, &ap);
+    if (fragment_len < 0 || s21_append_fragment(&out, fragment, fragment_len) < 0) {
+      va_end(ap);
+      return -1;
     }
   }
-  
-  *str = '\0';
+
+  *out = '\0';
   va_end(ap);
-  return str - start;
+  return (int)(out - start);
 }
